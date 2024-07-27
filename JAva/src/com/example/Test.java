8f19bd5af8743527edb1693b2b352e1a50b2f9dc
Surajss2290 <surajsuryawanshi2290@gmail.com>
2024-07-27 23:42:09 +0530

    at 11.42

--- JAva/src/com/example/Test.java
+++ JAva/src/com/example/Test.java
@@ -2,9 +2,8 @@ package com.example;
 
 public class Test {
 public static void main(String[]args) {
-	System.out.println("Hello suraj");
 	
-	System.out.println(" helli harsh New code");
+	System.out.println(" Replect only in develop");
 	
 }
 	
