aa1541c9a27220785007b70958aa61d5972b4df5
Surajss2290 <surajsuryawanshi2290@gmail.com>
2024-07-27 16:59:25 +0530

    new  second code

--- JAva/src/com/example/Test.java
+++ JAva/src/com/example/Test.java
@@ -4,7 +4,7 @@ public class Test {
 public static void main(String[]args) {
 	System.out.println("Hello suraj");
 	
-	System.out.println("New code");
+	System.out.println(" helli suraj New code");
 }
 	
 }
